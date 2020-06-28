fd35157eb0d45934fa7808eccf8a040cbd7243f9
Falkaniere <jonatasfalkaniere@gmail.com>
2020-06-28 01:06:42 -0300

    styling pages of repositorys and con with api

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,55 +1,103 @@
-import React from 'react';
-import { FiChevronRight }  from 'react-icons/fi'
+import React, { useState, useEffect, FormEvent } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
 import logoImage from '../../assets/logo.svg';
-import {Title, Form, Repositories} from './style'
+import { Title, Form, Repositories, Error } from './style';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storageRepositories = localStorage.getItem(
+      '@GituhubExplorer: repositories',
+    );
+
+    if (storageRepositories) {
+      return JSON.parse(storageRepositories);
+    } else {
+      return [];
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GituhubExplorer: repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+    if (!newRepo) {
+      setInputError('Digite autor/nome do repositorio');
+      return;
+    }
+    try {
+      //consumir api
+      const response = await api.get(`repos/${newRepo}`);
+
+      //armazena as info na const respository
+      const repository = response.data;
+
+      //salvar repositoru=io
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro na busca apor esse repositorio');
+    }
+  }
+
   return (
     <>
-       <img src={logoImage} alt="Github explorer" />
+      <img src={logoImage} alt="Github explorer" />
       <Title>Explore repositorios no Github</Title>
 
-      <Form action="" >
-        <input placeholder="Digite aqui o nome do repositório" />
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite aqui o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repositories>
-        <a href="teste">
-          <img
-           src="https://avatars0.githubusercontent.com/u/39073602?s=460&u=f705940c1955c045b09f57c3779f3eb8a80441c3&v=4"
-           alt="Jonatas"/>
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>Descrição de um repositorio dasda asdansdasd. asf aslknd as</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-           src="https://avatars0.githubusercontent.com/u/39073602?s=460&u=f705940c1955c045b09f57c3779f3eb8a80441c3&v=4"
-           alt="Jonatas"/>
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>Descrição de um repositorio dasda asdansdasd. asf aslknd as</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-           src="https://avatars0.githubusercontent.com/u/39073602?s=460&u=f705940c1955c045b09f57c3779f3eb8a80441c3&v=4"
-           alt="Jonatas"/>
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>Descrição de um repositorio dasda asdansdasd. asf aslknd as</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map((repository) => (
+          <Link
+            key={repository.full_name}
+            to={`/repositories/${repository.full_name}`}
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
       </Repositories>
     </>
   );
-
 };
 
 export default Dashboard;
